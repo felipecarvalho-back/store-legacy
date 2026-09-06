2e76fad0f7f0978ac42eef092ed522de0f20026e
felipecarvalho-back <felipedev.co@gmail.com>
2026-09-06 00:08:08 -0300

    feat: fazendo metodo pra lê as view

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
 class HomeController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index()
     {
-        parent::view();;
+        return $this->view('index');
     }
 }
--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -4,10 +4,29 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+
+use Slim\Psr7\Response;
+
 abstract class Controller
 {
-    protected static function view()
+    protected function view(string $view, array $dados = []): Response
     {
-        echo "entrou no view";
+        $response = new Response();
+        $arquivo = dirname(__DIR__) . "/view/{$view}.php";
+
+        if (!file_exists($arquivo)) {
+            $response->getBody()->write("View '{$view}' não encontrada.");
+            return $response->withStatus(404);
+        }
+
+        extract($dados);
+
+        ob_start();
+        require $arquivo;
+        $conteudo = (string) ob_get_clean();
+
+        $response->getBody()->write($conteudo);
+
+        return $response;
     }
 }
